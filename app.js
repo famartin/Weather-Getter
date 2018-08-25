aaf5a59293180c2f9e8292db09b76e12472ec13a
Faustino Martinez <faustmtz@protonmail.com>
2018-08-25 12:14:25 -0700

    node-fetch added

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var app = express();
 
 app.get('/', (req, res) => {
-	res.send('hello');
+	fetch('https://openweathermap.org/data/2.5/weather?q=Houston,us&appid=b6907d289e10d714a6e88b30761fae22')
+		.then((data) => {
+			return (data.json());
+		})
+		.then((json) => {
+			res.send((JSON.stringify(json)));
+		});
 });
 
 var http = require('http').Server(app);
