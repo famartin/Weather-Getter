95dfafc1bf912aca0681662f5a088baa175567bd
Faustino Martinez <faustmtz@protonmail.com>
2018-08-26 14:59:34 -0700

    Text only appears on Submit

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -13,7 +13,8 @@ class Weather extends Component {
 			desc: undefined,
 			humidity: undefined,
 			country: undefined,
-			city: undefined
+			city: undefined,
+			error: undefined
 		};
 	}
 
@@ -26,16 +27,32 @@ class Weather extends Component {
 			})
 			.then(data => {
 				console.log(data);
-				this.setState({
-					temp: data.main.temp,
-					high: data.main.temp_max,
-					low: data.main.temp_min,
-					state: data.weather[0].main,
-					desc: data.weather[0].description,
-					humidity: data.main.humidity,
-					country: data.sys.country,
-					city: data.name
-				});
+				if (data.cod == 200) {
+					this.setState({
+						temp: data.main.temp,
+						high: data.main.temp_max,
+						low: data.main.temp_min,
+						state: data.weather[0].main,
+						desc: data.weather[0].description,
+						humidity: data.main.humidity,
+						country: data.sys.country,
+						city: data.name,
+						error: undefined
+					});
+				}
+				else {
+					this.setState({
+						temp: undefined,
+						high: undefined,
+						low: undefined,
+						state: undefined,
+						desc: undefined,
+						humidity: undefined,
+						country: undefined,
+						city: undefined,
+						error: data.message
+					});
+				}
 			});
 	}
 
@@ -44,13 +61,39 @@ class Weather extends Component {
 			<div className="Weather">
 				<Form getWeather={this.getWeather} />
 				<h1>Weather</h1>
-				<p>Temperature: {this.state.temp}</p>
-				<p>High: {this.state.high}</p>
-				<p>Low: {this.state.low}</p>
-				<p>Humidity: {this.state.humidity}%</p>
-				<p>Status: {this.state.state}</p>
-				<p>Description: {this.state.desc}</p>
-				<p>Location: {this.state.city}, {this.state.country}</p>
+				{
+					this.state.city &&
+					this.state.country &&
+					<p>Location: {this.state.city}, {this.state.country}</p>
+				}
+				{
+					this.state.temp &&
+					<p>Temperature: {this.state.temp}&#176; F</p>
+				}
+				{
+					this.state.high &&
+					<p>High: {this.state.high}&#176; F</p>
+				}
+				{
+					this.state.low &&
+					<p>Low: {this.state.low}&#176; F</p>
+				}
+				{
+					this.state.humidity &&
+					<p>Humidity: {this.state.humidity}%</p>
+				}
+				{
+					this.state.state &&
+					<p>Status: {this.state.state}</p>
+				}
+				{
+					this.state.desc &&
+					<p>Description: {this.state.desc}</p>
+				}
+				{
+					this.state.error &&
+					<p>{this.state.error}</p>
+				}
 			</div>
 		);
 	}
