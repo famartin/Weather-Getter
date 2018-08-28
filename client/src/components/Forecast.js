c9303a1dd03e91dc842bc5e58402d5924e08cb05
Faustino Martinez <faustmtz@protonmail.com>
2018-08-27 20:17:10 -0700

    Forecast seperated by days

--- client/src/components/Forecast.js
+++ client/src/components/Forecast.js
@@ -6,7 +6,12 @@ class Forecast extends Component {
 	constructor() {
 		super();
 		this.state = {
-			temp: []
+			day1: undefined,
+			day2: undefined,
+			day3: undefined,
+			day4: undefined,
+			day5: undefined,
+			city: undefined
 		}
 	}
 
@@ -20,11 +25,49 @@ class Forecast extends Component {
 			.then(data => {
 				console.log(data);
 				if (data.cod === "200") {
-					for(var i = 0;i < data.list.length;i++) {
+					this.setState({
+						day1: [],
+						day2: [],
+						day3: [],
+						day4: [],
+						day5: []
+					});
+
+					var i = 0;
+					while(i < 7) {
 						this.setState({
-							temp: [...this.state.temp, data.list[i].main.temp]
+							day1: [...this.state.day1, data.list[i]]
 						});
+						i++;
 					}
+					while(i < 15) {
+						this.setState({
+							day2: [...this.state.day2, data.list[i]]
+						});
+						i++;
+					}
+					while(i < 23) {
+						this.setState({
+							day3: [...this.state.day3, data.list[i]]
+						});
+						i++;
+					}
+					while(i < 31) {
+						this.setState({
+							day4: [...this.state.day4, data.list[i]]
+						});
+						i++;
+					}
+					while(i < 39) {
+						this.setState({
+							day5: [...this.state.day5, data.list[i]]
+						});
+						i++
+					}
+
+					this.setState({
+						city: data.city.name
+					});
 				} else {
 					this.setState({
 						temp: undefined
@@ -37,8 +80,48 @@ class Forecast extends Component {
 		return (
 			<div style={{border: '1px solid black'}} className="Forecast">
 				<Form getData={this.getForecast} />
-				<h1>Forecast</h1>
-				<p>Temp:{this.state.temp[0]}</p>
+				<h1>5 Day Forecast</h1>
+				{this.state.city && <p>Location: {this.state.city}</p>}
+				{this.state.day1 && <p>Day 1:</p>}
+				{this.state.day1 && this.state.day1.map(obj => {
+					return (
+						<div key={obj.dt}>
+							<p>{obj.dt_txt} -- {obj.main.temp}</p>
+						</div>
+					);
+				})}
+				{this.state.day2 && <p>Day 2:</p>}
+				{this.state.day2 && this.state.day2.map(obj => {
+					return (
+						<div key={obj.dt}>
+							<p>{obj.dt_txt} -- {obj.main.temp}</p>
+						</div>
+					);
+				})}
+				{this.state.day3 && <p>Day 3:</p>}
+				{this.state.day3 && this.state.day3.map(obj => {
+					return (
+						<div key={obj.dt}>
+							<p>{obj.dt_txt} -- {obj.main.temp}</p>
+						</div>
+					);
+				})}
+				{this.state.day4 && <p>Day 4:</p>}
+				{this.state.day4 && this.state.day4.map(obj => {
+					return (
+						<div key={obj.dt}>
+							<p>{obj.dt_txt} -- {obj.main.temp}</p>
+						</div>
+					);
+				})}
+				{this.state.day5 && <p>Day 5:</p>}
+				{this.state.day5 && this.state.day5.map(obj => {
+					return (
+						<div key={obj.dt}>
+							<p>{obj.dt_txt} -- {obj.main.temp}</p>
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
